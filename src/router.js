259c430d17adbd3a1942a3db365aabd7f38e747e
xxm <15007591989@139.com>
2020-01-07 11:58:53 +0800

    了解Vue Router是如何设置的？

--- src/router.js
+++ src/router.js
@@ -3,3 +3,21 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 
 Vue.use(Router);
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      //route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for th
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>this is about page</h1>
+    <img src alt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
