b27bdb63011c073a54ff6fb266bfa37cac9fa59a
femaimi <femaimi9527@gmail.com>
2020-01-08 14:29:24 +0800

    creating routers finished

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <div class="nav">
-      <router-link to="/">Home</router-link>-|
-      <router-link to="/brazil">Brazil</router-link>-|
-      <router-link to="/panama">Panama</router-link>-|
-      <router-link to="/hawaii">Hawaii</router-link>-|
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/brazil">Brazil</router-link>
+      <router-link to="/panama">Panama</router-link>
+      <router-link to="/hawaii">Hawaii</router-link>
       <router-link to="/jamaica">Jamaica</router-link>
     </div>
     <router-view />
@@ -26,5 +26,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: 2c3e50;
+  padding: 0 10px;
+}
+#nav a.router-link-exact-active {
+  color: 42b983;
 }
 </style>
